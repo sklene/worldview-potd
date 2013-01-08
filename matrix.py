10bf4c729265debae9dda83cfb2642be490b13bb
Kevin Murray <k.d.murray.91@gmail.com>
2013-01-08 23:34:47 +1100

    matrix.py seems to work

--- matrix.py
+++ matrix.py
@@ -6,17 +6,23 @@ site = ("http://map1b.vis.earthdata.nasa.gov/wmts-geo/wmts.cgi?TIME=2013-01-08"
         "CorrectedReflectance_TrueColor&STYLE=&TILEMATRIXSET=EPSG4326_250m"
         "&TILEMATRIX=%i&TILEROW=%i&TILECOL=%i&FORMAT=image%%2Fjpeg")
 
-for matrix_num in xrange(5,6):
-    for x in xrange(20):
-        for y in xrange(20):
+for matrix_num in xrange(8,9):
+    for x in xrange(85, 115):
+        for y in xrange(260,300):
             handle = urllib2.urlopen(site % (matrix_num, x, y))
-            img = handle.read()
-            handle.close()
-            if hashlib.md5(img).hexdigest() != \
-                    "733091d2386fcedfe5a37f985725d543":
-
-                fn = "matrix_%i_%i_%i.jpeg" % (matrix_num, x, y)
+            fn = "img/matrix_%i_%i_%i.jpeg" % (matrix_num, x, y)
+            try:
+                img = handle.read()
+                handle.close()
+            except:
+                next
+            if True: #len(img) > 5000:  # Hack, i.e. is it not just black pixels
                 with open(fn, "w") as fh:
-                    fh.write(handle.read())
+                    fh.write(img)
                 print "done %s" % fn
+            else:
+                print fn + " empty"
+
+                
+
             
