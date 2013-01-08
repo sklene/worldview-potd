b671bb6b6dfe4a40f0d1053046b5f070d3781078
Kevin Murray <k.d.murray.91@gmail.com>
2013-01-08 23:58:40 +1100

    get_australia.sh not working, may ditch bash if python works

--- get_australia.sh
+++ get_australia.sh
@@ -17,9 +17,10 @@ do
         link="${link}1.0.0&LAYER=MODIS_Terra_CorrectedReflectance_TrueColor&"
         link="${link}STYLE=&TILEMATRIXSET=EPSG4326_250m&TILEMATRIX=${zoom}"
         link="${link}&TILEROW=${x}&TILECOL=${y}&FORMAT=image%%2Fjpeg"
-        wget "$link" -O "img/matrix_${zoom}_${x}_${y}.jpg"
+        echo $link
+        wget "$link" -q -O "img/matrix_${zoom}_${x}_${y}.jpeg"
     done
-    convert matrix_${zoom}_${x}_* +append row_${zoom}_${x}.jpg
+    convert matrix_${zoom}_${x}_* +append row_${zoom}_${x}.jpeg
 done
 convert row_${zoom}_* -append image_${zoom}.jpg
 
